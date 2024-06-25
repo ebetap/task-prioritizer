b4b6f3a4b2de92474b076275c0ebb1203e9c2753
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-25 18:53:43 +0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
+import TaskPrioritizer from '../src/index.js';
 
+const taskPrioritizer = new TaskPrioritizer();
+taskPrioritizer.addTask('Menulis laporan', 5, 3);
+taskPrioritizer.addTask('Rapat dengan tim', 4, 5);
+taskPrioritizer.addTask('Belajar materi baru', 3, 2);
+taskPrioritizer.addTask('Membalas email', 2, 4);
+
+console.log(taskPrioritizer.getTasks());
