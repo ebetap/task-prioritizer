1ceb423816492b861f00298ce7c23d5f588e8f67
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-25 19:37:41 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,49 @@ class TaskPrioritizer {
         this.tasks = [];
     }
 
-    addTask(name, importance, urgency) {
-        const task = { name, importance, urgency, score: importance + urgency };
+    addTask(name, importance, urgency, dueDate = null) {
+        if (this.tasks.some(task => task.name === name)) {
+            throw new Error('Task with this name already exists.');
+        }
+        const task = { name, importance, urgency, dueDate, score: importance + urgency, completed: false };
         this.tasks.push(task);
         this.prioritizeTasks();
     }
 
-    prioritizeTasks() {
-        this.tasks.sort((a, b) => b.score - a.score);
+    prioritizeTasks(customSort = null) {
+        if (customSort) {
+            this.tasks.sort(customSort);
+        } else {
+            this.tasks.sort((a, b) => {
+                if (a.score === b.score) {
+                    if (a.dueDate && b.dueDate) {
+                        return new Date(a.dueDate) - new Date(b.dueDate);
+                    } else if (a.dueDate) {
+                        return -1;
+                    } else if (b.dueDate) {
+                        return 1;
+                    }
+                    return a.name.localeCompare(b.name);
+                }
+                return b.score - a.score;
+            });
+        }
     }
 
-    getTasks() {
-        return this.tasks;
+    completeTask(name) {
+        const task = this.tasks.find(task => task.name === name);
+        if (task) task.completed = true;
+    }
+
+    deleteTask(name) {
+        this.tasks = this.tasks.filter(task => task.name !== name);
+    }
+
+    getTasks(includeCompleted = false) {
+        if (includeCompleted) {
+            return this.tasks;
+        }
+        return this.tasks.filter(task => !task.completed);
     }
 }
 
