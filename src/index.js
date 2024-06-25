eeeba3c150ba463e132b5ccd93797646457c8226
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-25 18:52:43 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+class TaskPrioritizer {
+    constructor() {
+        this.tasks = [];
+    }
 
+    addTask(name, importance, urgency) {
+        const task = { name, importance, urgency, score: importance + urgency };
+        this.tasks.push(task);
+        this.prioritizeTasks();
+    }
+
+    prioritizeTasks() {
+        this.tasks.sort((a, b) => b.score - a.score);
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+}
+
+export default TaskPrioritizer;
